57c494b05b010fb4dddbd2027a52706ce5c93b44
a <a@as-MacBook-Pro.local>
2021-06-20 11:58:15 -0400

    Serve well-known ssl cert info for letsencrypt

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,13 @@ import express from 'express';
 // rest of the code remains same
 const app = express();
 const PORT = 8000;
+
+app.use(express.static(`${__dirname}/static`, { dotfiles: 'allow' }))
+
+// - routes
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT} and`
+    + `\nServing static assets from '${__dirname}/static'`);
 });
