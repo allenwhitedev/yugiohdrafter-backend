0e1f283c0252f7ce0b78d9863faf82282868e42a
a <a@as-MacBook-Pro.local>
2021-06-20 13:27:27 -0400

    Setup SSL using letsencrypt when in production

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,37 @@
-import express from 'express';
+import express from 'express'
+import fs from 'fs'
+import https from 'https'
 
 const isProductionEnv = process.env.NODE_ENV === 'production' 
 
-// rest of the code remains same
-const app = express();
-const PORT = isProductionEnv ? 80 : 8000;
-
+// - initialize express server, serve .well-known file for letencrypt cert verification
+const app = express()
 app.use(express.static(`${__dirname}/static`, { dotfiles: 'allow' }))
 
 // - routes
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+app.get('/', (req, res) => res.send('Express + TypeScript Server'))
 
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT} and`
-    + `\nServing static assets from '${__dirname}/static'`);
-});
+// - start application
+const PORT = isProductionEnv ? 443 : 8000
+const envString = isProductionEnv ? 'Production' : 'Development'
+const serverStartMessage = `⚡️[server]: ${envString} server is running at https://localhost:${PORT} and`
++ `\nServing static assets from '${__dirname}/static'`
+if (isProductionEnv) 
+{
+  const letsencryptDomainPath = '/etc/letsencrypt/live/yugiohdrafter.com'
+  https
+  .createServer(
+    {
+      key: fs.readFileSync(`${letsencryptDomainPath}/privkey.pem`),
+      cert: fs.readFileSync(`${letsencryptDomainPath}/cert.pem`),
+      ca: fs.readFileSync(`${letsencryptDomainPath}/chain.pem`),
+    },
+    app
+  )
+  .listen(PORT, () => { console.log(serverStartMessage) })
+}
+else 
+{
+  app.listen(PORT, () => { console.log(serverStartMessage) })
+}
+
